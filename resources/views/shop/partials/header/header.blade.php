945fbb3cfae6eb37b380a23751c6106db513213c
perawita <perawitayasa@gmail.com>
2024-09-16 19:03:50 +0800

    update sizee, add new rules, update configuration

--- resources/views/shop/partials/header/header.blade.php
+++ resources/views/shop/partials/header/header.blade.php
@@ -55,14 +55,14 @@
             <!-- Search Bar -->
             <div class="flex-1">
                 <input type="text" id="search-enggine" placeholder="Search products..."
-                    class="w-full px-3.5 py-1.5 md:px-4 md:py-2 hover:ring-blue-600 border rounded-lg focus:outline-none focus:ring-1 focus:ring-blue-700">
+                    class="w-full px-4 py-2 hover:ring-blue-600 border rounded-lg focus:outline-none focus:ring-1 focus:ring-blue-700">
             </div>
 
             <!-- Cart Buttons -->
             <a id="cart-btn" href={{ route('shoping.cart.detail', ['id_cart' => 'py']) }}
                 class="text-gray-600 focus:outline-none">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                    class="bi bi-cart3 w-7 h-7 md:w-8 md:h-8" viewBox="0 0 16 16">
+                    class="bi bi-cart3 w-8 h-8" viewBox="0 0 16 16">
                     <path
                         d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
                 </svg>
@@ -71,7 +71,7 @@
             <!-- Users Buttons -->
             <button id="user-btn" class="text-gray-600 focus:outline-none">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                    class="bi bi-person-circle w-7 h-7 md:w-8 md:h-8" viewBox="0 0 16 16">
+                    class="bi bi-person-circle w-8 h-8" viewBox="0 0 16 16">
                     <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                     <path fill-rule="evenodd"
                         d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
